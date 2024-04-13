41b6db377d9752e5a199c60197b934b47b9c819e
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2024-04-13 23:48:01 +0530

    update js

--- script.js
+++ script.js
@@ -5,8 +5,20 @@ let turn = "X";
 const turnchange = () => {
   return turn === "O" ? "X" : "O";
 };
-
+//function to winner
 const winner = () => {};
 
 //logic of game
-let boxes = document.getElementById;
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach((element) => {
+  let boxtext = document.querySelector(".boxtext");
+  b.addEventListener("click", () => {
+    if (boxtext.innerText === "") {
+      boxtext.innerText = "0";
+      turnchange();
+      sound.play();
+      winner();
+      document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+    }
+  });
+});
