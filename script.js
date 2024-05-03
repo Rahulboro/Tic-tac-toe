77c68437af1c260c5c7b013f6902c519af9d79ec
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2024-05-03 11:28:21 +0530

    Remove audio files; update script.js - remove unused audio, add turn tracking, start of winner function

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 console.log(`welcome to tic tac toes`);
-let sound = new Audio("gamesound.mp3");
+let turnsound = new Audio("poping.mp3");
+let gameOversound = new Audio("poping.mp3");
+let lineThrough = new Audio("poping.mp3");
 let turn = "X";
 
 const turnchange = () => {
-  return turn === "O" ? "X" : "O";
+  return turn === "X" ? "0" : "X";
 };
 //function to winner
-const winner = () => {};
+const winner = () => {
+  let boxtext = document.querySelector("boxtext");
+  let gamewin = [
+    [0, 1, 2][(3, 4, 6)][(0, 1, 2)][(0, 1, 2)][(0, 1, 2)][(0, 1, 2)][(0, 1, 2)][
+      (0, 1, 2)
+    ][(0, 1, 2)][(0, 1, 2)][(0, 1, 2)],
+  ];
+};
 
 //logic of game
 let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach((element) => {
-  let boxtext = document.querySelector(".boxtext");
-  b.addEventListener("click", () => {
+  let boxtext = element.querySelector(".boxtext");
+  element.addEventListener("click", (e) => {
     if (boxtext.innerText === "") {
-      boxtext.innerText = "0";
-      turnchange();
-      sound.play();
+      boxtext.innerText = turn;
+      turn = turnchange();
+      turnsound.play();
       winner();
       document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
     }
